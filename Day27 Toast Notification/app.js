acde76778a0cc4b0b74d97d11d600a42c748de59
jaatcodes <97509813+jaatcodes@users.noreply.github.com>
2022-04-20 19:26:15 +0530

    Day27 ToastNotification

---
+++ Day27 Toast Notification/app.js
@@ -0,0 +1,29 @@
+const button = document.getElementById('button')
+const toasts = document.getElementById('toasts')
+
+const messages = ['Message One', 'Message Two', 'Message Three', 'Message Four']
+const MessageType = ['error', 'success', 'info']
+
+button.addEventListener('click', () => createNotification())
+
+function createNotification(message = null, type = null) {
+	const notif = document.createElement('div')
+	notif.classList.add('toast')
+	notif.classList.add(type ? type : getRandomMessageType())
+
+	notif.innerText = message ? message : getRandomMessage()
+
+	toasts.appendChild(notif)
+
+	setTimeout(() => {
+		notif.remove()
+	}, 3000)
+}
+
+function getRandomMessage() {
+	return messages[Math.floor(Math.random() * messages.length)]
+}
+
+function getRandomMessageType() {
+	return MessageType[Math.floor(Math.random() * MessageType.length)]
+}
